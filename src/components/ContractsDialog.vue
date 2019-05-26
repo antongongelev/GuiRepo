aca5bc1630b4eef3ddc6b7dc477a9eeb73d17bc8
Anton <antongongelev@gmail.com>
2019-05-27 02:24:32 +0300

    CRUD for contracts

--- src/components/ContractsDialog.vue
+++ src/components/ContractsDialog.vue
@@ -8,7 +8,7 @@
             <h4 slot="form-text">Are you sure that you want to delete this contract?</h4>
         </confirmation-dialog>
 
-        <v-dialog v-model="dialog" persistent max-width="500">
+        <v-dialog v-model="dialog" persistent max-width="600">
             <v-card>
                 <v-card-title>
                     <h2>{{this.client.firstName}} {{this.client.secondName}}'s contracts</h2>
@@ -23,12 +23,13 @@
                         </v-icon>
                         <v-icon
                                 color="orange"
-                                @click=""
+                                @click="editContract(contract)"
                         >
                             edit
                         </v-icon>
                         <strong>Phone number:</strong> {{contract.phoneNumber}}
                         <strong>, Tariff:</strong> {{contract.tariff.name}}
+                        <strong>, Payment:</strong> {{contract.tariff.payment}}
 
                     </li>
                 </v-card-text>
@@ -59,33 +60,30 @@
         },
         methods: {
             openContractsDialog(client) {
+                console.log(client);
                 this.contracts = Object.assign({}, client.contracts);
                 this.client = Object.assign({}, client);
-                console.log(client);
                 this.dialog = true;
             },
             addContract() {
-                this.dialog = false;
-                this.$refs.newContractDialog.open(this.client);
+                this.$refs.newContractDialog.open(true, this.client);
             },
-            newContract() {
-                this.$emit('newContract');
+            editContract(contract) {
+                this.$refs.newContractDialog.open(false, this.client, contract);
+            },
+            newContract(contract) {
+                this.$emit('newContract', contract);
+                this.dialog = false;
             },
             openConfirmationDialog(contract) {
                 this.$refs.ConfirmationDialog.openConfirmationDialogForDeletingContract(contract)
             },
             deleteContract(contract) {
                 this.$axios.post('http://localhost:8090/contracts/delete', contract)
-                    .then(
-                        this.$emit('newContract')
-                    )
-                    .catch(error => {
-                        console.log(error.response);
-                    })
-                    .finally(() => {
-                        this.dialog = false
+                    .then(response => {
+                        this.$emit('newContract', response.data);
                     });
-
+                this.dialog = false;
             }
         }
     }
--- src/components/NewContractDialog.vue
+++ src/components/NewContractDialog.vue
@@ -1,21 +1,46 @@
-<template>
+<template xmlns:v-slot="http://www.w3.org/1999/XSL/Transform">
 
     <v-dialog max-width="500" persistent v-model="dialog">
 
         <v-card>
             <v-card-title>
-                <h2>Add new contract to {{this.client.firstName}} {{this.client.secondName}}</h2>
+                <h2>{{isNew? 'Add new contract to ' + this.client.firstName + ' ' + this.client.secondName : 'Edit ' +
+                    this.client.firstName + ' ' + this.client.secondName + '\'s contract'}}</h2>
             </v-card-title>
             <v-form>
                 <v-card-text>
                     <v-text-field color="orange" label="Phone number"
                                   v-model="newContract.phoneNumber"></v-text-field>
+                    <v-menu offset-y>
+                        <template v-slot:activator="{ on }">
+                            <v-card-text>
+                                Chosen Tariff:
+                                <v-btn
+                                        outline
+                                        small
+                                        color="orange"
+                                        v-on="on"
+                                >
+                                    {{ chosenTariffName }}
+                                </v-btn>
+                            </v-card-text>
+
+                        </template>
+                        <v-list>
+                            <v-list-tile
+                                    v-for="tariff in tariffs"
+                                    @click="newTariff(tariff)"
+                            >
+                                <v-list-tile-title>{{ tariff.name }}</v-list-tile-title>
+                            </v-list-tile>
+                        </v-list>
+                    </v-menu>
                 </v-card-text>
             </v-form>
             <v-card-actions>
                 <v-spacer></v-spacer>
                 <v-btn flat color="black" @click="close">Close</v-btn>
-                <v-btn flat color="orange" @click="saveNewContract">Save</v-btn>
+                <v-btn flat color="orange" @click="saveNewContract(isNew)">{{isNew? 'Add' : 'Edit'}}</v-btn>
             </v-card-actions>
         </v-card>
 
@@ -26,7 +51,6 @@
 
 <script>
 
-
     export default {
         name: "NewContractDialog",
         data() {
@@ -35,32 +59,59 @@
                 client: {},
                 newContract: {},
                 tariffs: {},
+                chosenTariffName: 'None',
+                isNew: '',
             }
         },
         methods: {
-            open(client) {
+            newTariff(tariff) {
+                this.chosenTariffName = tariff.name;
+                this.newContract.tariff = tariff;
+            },
+            open(isNew, client, contract) {
+                this.isNew = isNew;
                 this.client = Object.assign({}, client);
+                if (isNew) {
+                    this.chosenTariffName = 'None';
+                    this.newContract.phoneNumber = '';
+                } else {
+                    this.chosenTariffName = contract.tariff.name;
+                    this.newContract.phoneNumber = contract.phoneNumber;
+                    this.newContract.id = contract.id;
+                }
                 this.dialog = true;
             },
             close() {
-                this.newContract.phoneNumber = '';
                 this.dialog = false;
             },
-            saveNewContract() {
+            saveNewContract(isNew) {
                 this.newContract.client = this.client;
-                this.newContract.tariff = this.tariffs[0]; //TODO: Admin should be able to chose tariff
                 console.log(this.newContract);
-                this.$axios.post('http://localhost:8090/contracts/create', this.newContract)
-                    .then(
-                        this.$emit('newContract')
-                    )
-                    .catch(error => {
-                        console.log(error.response);
-                    })
-                    .finally(() => {
-                        this.newContract.phoneNumber = '';
-                        this.dialog = false
-                    });
+                if (isNew) {
+                    this.$axios.post('http://localhost:8090/contracts/create', this.newContract)
+                        .then(response => {
+                                this.$emit('newContract', response.data);
+                            }
+                        )
+                        .catch(error => {
+                            console.log(error.response);
+                        })
+                        .finally(() => {
+                            this.dialog = false
+                        });
+                } else {
+                    this.$axios.post('http://localhost:8090/contracts/update', this.newContract)
+                        .then(response => {
+                                this.$emit('newContract', response.data);
+                            }
+                        )
+                        .catch(error => {
+                            console.log(error.response);
+                        })
+                        .finally(() => {
+                            this.dialog = false
+                        });
+                }
             }
         },
         mounted() {
